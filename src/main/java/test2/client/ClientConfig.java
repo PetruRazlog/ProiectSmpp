ec3f46a00680f30ce9fd156f6ae5c630ea58ded2
p.razlog <p.razlog@unifun.com>
2021-03-05 14:24:21 +0200

    adjust the logging

--- src/main/java/test2/client/ClientConfig.java
+++ src/main/java/test2/client/ClientConfig.java
@@ -1,20 +1,16 @@
 package test2.client;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 
-public @Data class ClientConfig {
+@Data
+@AllArgsConstructor
+public class ClientConfig {
 
     private String systemId;
     private String password;
     private String host;
     private int port;
-
-    public ClientConfig(String systemId, String password, String host, int port) {
-        this.systemId = systemId;
-        this.password = password;
-        this.host = host;
-        this.port = port;
-    }
 
 }
